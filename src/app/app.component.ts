5b7acb96df745e6c4a4ae0c5cb81ba09fd8cdd2d
Giovanni Tapia <ryukaj1_@hotmail.com>
2022-12-29 09:15:54 -0600

    Proyecto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre : string  = 'giovanni';
+  valor : number  = 23224;
+  
+  obj = {
+    nombre: this.nombre,
+    valor: this.valor
+  }
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//CAMBIAR EL LOCALE DE LA APP
+import localeEs from "@angular/common/locales/es-MX";
+import localeFr from "@angular/common/locales/fr";
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue : 'es-MX' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
